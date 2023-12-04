c5f81d0574569a7cbca6717fbe72fa6ba00d3047
Darren Griffin <darren.griffin@live.co.uk>
2023-12-04 23:44:27 +0000

    Changed pinia initialisation so it can be extended later

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,13 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
 
 import { useAuthStore } from './stores/auth'
 const authStore = useAuthStore()
+
