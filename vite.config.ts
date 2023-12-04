54c6421fd3219754dbc7075cdd493a2575c4ef02
Darren Griffin <darren.griffin@live.co.uk>
2023-12-04 22:43:47 +0000

    Changed GitHub actions base implementation method

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
+let base = '/';
+// if process.env.GITHUB_ACTION exists, set the base so it works on GitHub pages
+if (process.env.GITHUB_ACTION) {
+  console.log('We\'re in a GitHub Action! Changing base to /site-manager-ui/');
+  base = '/site-manager-ui/';
+}
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     basicSsl()
   ],
+  base,
   envPrefix: 'UI_',
   resolve: {
     alias: {
