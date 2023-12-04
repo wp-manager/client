ff1439c363e3e29f2be977a946b0eb3f01795586
Darren Griffin <darren.griffin@live.co.uk>
2023-12-04 23:05:30 +0000

    Fixed incorrect assignment of env var

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,11 @@ import vue from "@vitejs/plugin-vue";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
 let base = "/";
+let isGitHubAction = false;
 // if process.env.GITHUB_ACTION exists, set the base so it works on GitHub pages
 if (process.env.GITHUB_ACTION) {
-    console.log("We're in a GitHub Action! Changing base to /site-manager-ui/");
+  console.log("We're in a GitHub Action! Changing base to /site-manager-ui/");
+    isGitHubAction = true;
     base = "/site-manager-ui/";
 }
 
@@ -16,7 +18,7 @@ export default defineConfig({
     plugins: [vue(), basicSsl()],
     // Add import.meta.env.GITHUB_ACTION to the global scope
     define: {
-        "import.meta.env.GITHUB_ACTION": process.env.GITHUB_ACTION,
+        "import.meta.env.GITHUB_ACTION": isGitHubAction,
     },
     base,
     envPrefix: "UI_",
