6b9016dddd056721b607983440c0ef1d95a22999
Darren Griffin <darren.griffin@live.co.uk>
2023-12-04 23:13:58 +0000

    Final alterations to GitHub pages env changes

--- src/utils/wordpressAuth.ts
+++ src/utils/wordpressAuth.ts
@@ -1,12 +1,19 @@
+import router from "@/router";
 import { isValidURL, sanitiseURL } from "./url";
 
 class WordpressAuth {
     private appID: string = '6708b25c-f31a-4a99-8dc5-68d575514c08';
-    private callbackPath: string = '/sites/add/callback';
+    private callbackPath: string = '';
     public uri: string = '';
-
+    
     constructor(url: string) {
+        // get router history mode
         if(!isValidURL(url)) return;
+
+        // Set the callback path
+        this.callbackPath = router.resolve({
+            name: 'add-site-callback'
+        }).href;
 
         this.uri = sanitiseURL(url);
     }
