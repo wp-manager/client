d2b9950ecbbc97960e533733fe26fdb7bd7cc458
Darren <darren.griffin@live.co.uk>
2023-12-16 02:13:30 +0000

    Reformat

--- src/stores/api.ts
+++ src/stores/api.ts
@@ -1,13 +1,13 @@
-import { defineStore } from 'pinia'
-import { ref, watch, type Ref } from 'vue';
-import { useRoute, useRouter, type Router } from 'vue-router';
+import { defineStore } from "pinia";
+import { ref, watch, type Ref } from "vue";
+import { useRoute, useRouter, type Router } from "vue-router";
 
 type Site = {
-    _id: string,
-    uri: string
-}
+    _id: string;
+    uri: string;
+};
 
-export const useApiStore = defineStore('api', () => {
+export const useApiStore = defineStore("api", () => {
     const apiBase = import.meta.env.UI_API;
     let siteUri = ref("");
     let user = ref({});
@@ -17,31 +17,37 @@ export const useApiStore = defineStore('api', () => {
     let authCheck: (user: any) => void = () => {};
 
     getAuthdUser();
-    getSites().then(resp => sites.value = resp);
-    root().then(resp => rootData.value = resp);
+    getSites().then((resp) => (sites.value = resp));
+    root().then((resp) => (rootData.value = resp));
 
-    async function getAuthdUser(){
-        user.value = await fetch(`https://${apiBase}/auth/user`).then(res => res.json());
+    async function getAuthdUser() {
+        user.value = await fetch(`https://${apiBase}/auth/user`).then((res) =>
+            res.json()
+        );
         authCheck(user.value);
-    }    
-
-    async function getSites(): Promise<Site[]>{
-        return fetch(`https://${apiBase}/sites`).then(res => res.json());
     }
 
-    async function root(){
-        return fetch(`https://${apiBase}/site/${siteUri.value}/wp-json/`).then(res => res.json());
+    async function getSites(): Promise<Site[]> {
+        return fetch(`https://${apiBase}/sites`).then((res) => res.json());
     }
 
-    async function getRoute(route: string){
-        return fetch(`https://${apiBase}/site/${siteUri.value}/wp-json/${route}`).then(res => res.json());
+    async function root() {
+        return fetch(`https://${apiBase}/site/${siteUri.value}/wp-json/`).then(
+            (res) => res.json()
+        );
     }
 
-    function onAuth(callback: (user: any) => void){
+    async function getRoute(route: string) {
+        return fetch(
+            `https://${apiBase}/site/${siteUri.value}/wp-json/${route}`
+        ).then((res) => res.json());
+    }
+
+    function onAuth(callback: (user: any) => void) {
         authCheck = callback;
     }
 
-    return { getSites, onAuth, user, sites, rootData, getRoute, siteUri }
-})
+    return { getSites, onAuth, user, sites, rootData, getRoute, siteUri };
+});
 
 export type { Site };
