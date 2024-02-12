cc4390a431696fff16fa7e0d27777f9e12432fcf
Darren <darren.griffin@live.co.uk>
2024-02-12 23:18:21 +0000

    WIP: Added site header to site layout

--- src/views/Site.vue
+++ src/views/Site.vue
@@ -8,5 +8,23 @@ siteStore.routeSite()?.discover();
 </script>
 
 <template>
+	<div class="site-header border-bottom mb-3 pb-3 pt-1">
+		<h1 class="h2 lh-1 site-header__heading" :class="{
+			'text-muted opacity-25': siteStore.routeSite()?.discover().loading
+		}">{{ siteStore.routeSite()?.discover().data?.name || siteStore.routeSite()?.url }}
+		</h1>
+		<div class="site-header__actions">
+			
+		</div>
+	</div>
 	<RouterView />
 </template>
+
+<style scoped lang="scss">
+	.site-header{
+		margin-left: -1rem;
+		margin-right: -1rem;
+		padding-left: 1rem;
+		padding-right: 1rem;
+	}
+</style>
